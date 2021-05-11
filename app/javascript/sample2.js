b265ef0ccb91c70dfe9d8e5f33322cc0e0062113
yakyutarou <78419230+yakyutarou@users.noreply.github.com>
2021-05-11 18:00:59 +0900

    Objects.defineProperties関数

--- app/javascript/sample2.js
+++ app/javascript/sample2.js
@@ -18,3 +18,29 @@ console.log(myThings2.food)
 console.log(myThings2["food"])
 //プロパティが存在しない時undefinedと表示
 console.log(myThings2.japan)
+
+let myThings3 = {}
+
+Object.defineProperties(myThings3,{
+  sports:{
+    enumerable:true,
+    writable: true,
+    value:"サッカー"
+  },
+  food:{
+    enumerable: false,
+    writable: false,
+    value: "カレーライス"
+  }
+})
+
+// enumerable属性
+console.log(Object.keys(myThings3))
+
+// writable属性
+myThings3.sports = "ラグビー"
+console.log(myThings3.sports)
+
+myThings3.food = "寿司"       //writableにfalseが指定された場合は代入は無効になる。
+console.log(myThings3.food)
+
